8b86657fc5643b56e808f8dd858284b44dcfc4ed
22mantju <49483197+22mantju@users.noreply.github.com>
2019-04-10 12:39:16 -0400

    Add files via upload

---
+++ hell-world.py
@@ -0,0 +1,14 @@
+# Justin Mantis
+# 04/10/2019
+
+#Hello World
+#####################
+#    Hello World    #
+#                   #
+# Our First Python  #
+#    Application    #
+#####################
+
+#pring "Hello, World" to the Screen.
+print("Hello, World")
+
